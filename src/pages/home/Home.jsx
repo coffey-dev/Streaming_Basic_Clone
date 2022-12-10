39f09519fe2a59803a1344d74641743b6d0d8020
coffey-dev <scoffey@abc.gob.ar>
2022-12-09 21:00:48 -0300

    Finalicé la maqueta UI

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Navbar from "../components/navbar/Navbar";
-import Featured from '../components/featured/Featured';
-import List from '../components/List/List';
+import Navbar from "../../components/navbar/Navbar";
+import Featured from '../../components/featured/Featured';
+import List from '../../components/List/List';
 import "./home.scss";
 
 
